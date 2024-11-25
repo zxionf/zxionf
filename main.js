38f9d4fbda5d9e63487326cc7feedb7845ea2f0f
zxionf <3512608310@qq.com>
2024-11-25 12:42:59 +0800

    241125

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log('Hello World!');
+
+function openNav(){
+    document.getElementById("myNav").style.height = "100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.height = "0%";
+}
+
+// 高亮偏移
+// const highlightgrid = document.querySelector('.highlightgrid');
+const body = document.querySelector('body');
+const highlightcards = document.querySelectorAll('.highlightcard');
+
+body.onmousemove = (e) => {
+    for (const card of highlightcards) {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left - card.offsetWidth /2 ;
+        const y = e.clientY - rect.top - card.offsetHeight /2 ;
+        card.style.setProperty('--x',x+"px");
+        card.style.setProperty('--y',`${y}px`);
+        // console.log(card.offsetWidth);
+    }
+    // console.log(e.clientX);
+}
